af6cf43926022ceaf1a1847e02534043a9ecdd44
MILOUX Antoine <antoine.miloux@devinfluence.fr>
2020-02-28 16:17:03 +0100

    Init Vue

---
+++ src/layouts/components/TheFooter.vue
@@ -0,0 +1,30 @@
+<!-- =========================================================================================
+    File Name: TheFooter.vue
+    Description: Footer component
+    Component Name: TheFooter
+    ----------------------------------------------------------------------------------------
+    Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+      Author: Pixinvent
+    Author URL: http://www.themeforest.net/user/pixinvent
+========================================================================================== -->
+
+
+<template functional>
+  <footer class="the-footer flex-wrap justify-between" :class="classes">
+    <span class="md:flex hidden items-center">
+      <span>Fait par Jean Massip et Antoine Miloux & Avec le</span>
+      <feather-icon icon="HeartIcon" svgClasses="stroke-current text-danger w-6 h-6" class="ml-2" />
+    </span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "the-footer",
+  props: {
+    classes: {
+      type: String
+    }
+  }
+};
+</script>
---
+++ src/views/DashboardAnalytics.vue
@@ -0,0 +1,46 @@
+<!-- =========================================================================================
+  File Name: DashboardAnalytics.vue
+  Description: Dashboard Analytics
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+========================================================================================== -->
+
+<template>
+  <div id="dashboard-analytics"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {}
+};
+</script>
+
+<style lang="scss">
+#dashboard-analytics {
+  .greet-user {
+    position: relative;
+    .decore-left {
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+    .decore-right {
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  }
+
+  @media (max-width: 576px) {
+    .decore-left,
+    .decore-right {
+      width: 140px;
+    }
+  }
+}
+</style>
